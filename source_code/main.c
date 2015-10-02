39f3cae566fba2f9e33d450968130a899378a2fc
Eduard Soltuzu <eduard.soltuzu@outlook.com>
2015-10-02 11:33:17 +0300

    Update main.c
    Edited online

--- source_code/main.c
+++ source_code/main.c
@@ -11,28 +11,28 @@
 
 int main()
 {
-    uint8_t ModBus_message[LengModBusMsg]={0x01,0x03,0x00,0x04,0x00,0x04};
+	uint8_t ModBus_message[LengModBusMsg]={0x01,0x03,0x00,0x04,0x00,0x04};
 	uint16_t CRC_result;
 	
-    CRC_result = CRC16 (( uint8_t* )ModBus_message, 6 );
+	CRC_result = CRC16 (( uint8_t* )ModBus_message, 6 );
     
 	printf("CRC_result = 0x%X\r\n",CRC_result);
 	
-    uint8_t Hi = CRC_result >> 8;
-    uint8_t Lo = CRC_result & 0x00FF;
+	uint8_t Hi = CRC_result >> 8;
+	uint8_t Lo = CRC_result & 0x00FF;
 
-    ModBus_message[CRC_pos_Hi] = Hi;
-    ModBus_message[CRC_pos_Lo] = Lo;
+	ModBus_message[CRC_pos_Hi] = Hi;
+	ModBus_message[CRC_pos_Lo] = Lo;
 
 	int i=0;
-    for(i=0;i<8;i++)
-    {
-        printf("ModBus_message[%d] = %X\r\n",i,ModBus_message[i]);
-    }
-
-    return 1;
-
-    return 0;
+	for(i=0;i<8;i++)
+	{
+		printf("ModBus_message[%d] = %X\r\n",i,ModBus_message[i]);
+	}
+	
+	return 1;
+	
+	return 0;
 
 }
 
